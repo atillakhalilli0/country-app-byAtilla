17b1e64fbb8ad25ef01f5c7933dca64aec539a5a
atillakhalilli0 <atilla.xalilli@gmail.com>
2025-05-17 15:23:25 +0400

    nav width fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ country, setRegion }) => {
 
   return (
     <nav className="bg-white dark:bg-gray-900 shadow-sm fixed top-0 w-full z-10">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-[85%] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 flex items-center">
             <img className="h-8 w-8 spin-logo" src={logo} alt="logo" />
