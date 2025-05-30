a01ab040cea80078414f8daf18aeb2c052619442
atillakhalilli0 <atilla.xalilli@gmail.com>
2025-05-30 16:30:05 +0400

    added passing through borders feature

--- src/App.jsx
+++ src/App.jsx
@@ -34,33 +34,38 @@ function App() {
    const filteredCountries = region ? country.filter((item) => item.region === region) : country;
 
    return (
-      <div className="bg-[#eee] dark:bg-blue-950">
+      <div className="min-h-screen flex flex-col bg-[#eee] dark:bg-blue-950 text-gray-800 dark:text-white">
          <Navbar setRegion={setRegion} country={country} />
-         {location.pathname === "/" && <Hero country={country} rand={rand} setRand={setRand} search={search} setSearch={setSearch} show={show} showSection={showSection} haveaLook={haveaLook} />}
-         <Routes>
-            <Route path="/" element={<Home country={country} load={load} rand={rand} search={search} setRand={setRand} setSearch={setSearch} show={show} showSection={showSection} haveaLook={haveaLook} />} />
-            <Route path="/region/:region" element={<Home country={country} load={load} rand={rand} setRand={setRand} setSearch={setSearch} show={show} showSection={showSection} haveaLook={haveaLook} />} />
-            <Route path="/details/:alpha3Code" element={<CardDetail country={country} />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/notfound" replace />} />
-         </Routes>
-         {!location.pathname.startsWith("/details") && filteredCountries.length > load && (
-            <div className="flex items-center justify-center mt-10">
-               <button
-                  onClick={loadMore}
-                  className="relative group overflow-hidden py-3 px-10 rounded-2xl font-semibold text-white text-lg transition-all duration-300 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 dark:from-blue-900 dark:via-purple-900 dark:to-fuchsia-900 shadow-xl hover:shadow-fuchsia-500/60 hover:scale-105 focus:outline-none focus:ring-4 focus:ring-fuchsia-400/60"
-               >
-                  <span className="relative z-10 flex items-center gap-2">
-                     <svg className="w-5 h-5 text-white transition-transform duration-300 group-hover:rotate-180" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
-                     </svg>
-                     Load More
-                  </span>
-                  <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-10 transition-opacity duration-300"></div>
-                  <div className="absolute inset-0 w-full h-full transform scale-0 group-hover:scale-150 transition-transform duration-500 ease-out bg-white opacity-5 rounded-full"></div>
-               </button>
-            </div>
-         )}
+
+         <main className="flex-grow">
+            {location.pathname === "/" && <Hero country={country} rand={rand} setRand={setRand} search={search} setSearch={setSearch} show={show} showSection={showSection} haveaLook={haveaLook} />}
+
+            <Routes>
+               <Route path="/" element={<Home country={country} load={load} rand={rand} search={search} setRand={setRand} setSearch={setSearch} show={show} showSection={showSection} haveaLook={haveaLook} />} />
+               <Route path="/region/:region" element={<Home country={country} load={load} rand={rand} setRand={setRand} setSearch={setSearch} show={show} showSection={showSection} haveaLook={haveaLook} />} />
+               <Route path="/details/:alpha3Code" element={<CardDetail country={country} />} />
+               <Route path="/notfound" element={<NotFound />} />
+               <Route path="*" element={<Navigate to="/notfound" replace />} />
+            </Routes>
+
+            {!location.pathname.startsWith("/details") && filteredCountries.length > load && (
+               <div className="flex items-center justify-center mt-10">
+                  <button
+                     onClick={loadMore}
+                     className="relative cursor-pointer mb-5 group overflow-hidden py-3 px-10 rounded-2xl font-semibold text-white text-lg transition-all duration-300 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 dark:from-blue-900 dark:via-purple-900 dark:to-fuchsia-900 shadow-xl hover:shadow-fuchsia-500/60 hover:scale-105 focus:outline-none focus:ring-4 focus:ring-fuchsia-400/60"
+                  >
+                     <span className="relative z-10 flex items-center gap-2">
+                        <svg className="w-5 h-5 text-white transition-transform duration-300 group-hover:rotate-180" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+                        </svg>
+                        Load More
+                     </span>
+                     <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-10 transition-opacity duration-300"></div>
+                     <div className="absolute inset-0 w-full h-full transform scale-0 group-hover:scale-150 transition-transform duration-500 ease-out bg-white opacity-5 rounded-full"></div>
+                  </button>
+               </div>
+            )}
+         </main>
       </div>
    );
 }
--- src/Components/CardDetail.jsx
+++ src/Components/CardDetail.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useParams, Link } from "react-router-dom";
 
 function CardDetail({ country }) {
    const { alpha3Code } = useParams();
@@ -10,16 +9,47 @@ function CardDetail({ country }) {
    }
 
    return (
-      <div className="p-10 mt-30 max-w-3xl mx-auto bg-white dark:bg-gray-800 shadow-md rounded-lg">
-         <h2 className="text-3xl font-bold mb-4 text-gray-900 dark:text-white">{selectedCountry.name}</h2>
-         <img src={selectedCountry.flag} alt={selectedCountry.name} className="w-full h-64 object-cover rounded-lg mb-4" />
-         <p className="text-gray-700 dark:text-gray-300">
-            <strong>Capital:</strong> {selectedCountry.capital} <br />
-            <strong>Region:</strong> {selectedCountry.region} <br />
-            <strong>Borders:</strong> {selectedCountry.borders ? selectedCountry.borders.join(", ") : "None"} <br />
-            <strong>Population:</strong> {selectedCountry.population} <br />
-            <strong>Area:</strong> {selectedCountry.area} km²
-         </p>
+      <div className="min-h-screen flex items-center justify-center bg-[#f5f5f5] dark:bg-blue-950 px-4 py-10">
+         <div className="bg-white dark:bg-gray-800 shadow-2xl rounded-2xl overflow-hidden max-w-4xl w-full flex flex-col lg:flex-row">
+            <div className="w-full h-64 lg:h-auto">
+               <img src={selectedCountry.flag} alt={selectedCountry.name} className="w-full h-full object-cover" />
+            </div>
+
+            <div className="p-8 flex flex-col justify-center lg:w-1/2 relative">
+               <h2 className="text-4xl mt-5 font-extrabold text-gray-900 dark:text-white mb-6">{selectedCountry.name}</h2>
+
+               <ul className="space-y-3 text-gray-700 dark:text-gray-300 text-lg mb-6">
+                  <li>
+                     <span className="font-semibold">Capital:</span> {selectedCountry.capital}
+                  </li>
+                  <li>
+                     <span className="font-semibold">Region:</span> {selectedCountry.region}
+                  </li>
+                  <li>
+                     <span className="font-semibold">Borders:</span>{" "}
+                     <p className="flex flex-wrap gap-y-2">
+                        {selectedCountry.borders?.length
+                           ? selectedCountry.borders.map((border) => (
+                                <Link key={border} to={`/details/${border}`} className="inline-block text-sm font-medium bg-indigo-600 dark:bg-indigo-700 text-white dark:text-white px-3 py-1 rounded-full mr-2 hover:bg-indigo-500 dark:hover:bg-indigo-600 transition-colors duration-300">
+                                   {border}
+                                </Link>
+                             ))
+                           : "None"}
+                     </p>
+                  </li>
+                  <li>
+                     <span className="font-semibold">Population:</span> {selectedCountry.population}
+                  </li>
+                  <li>
+                     <span className="font-semibold">Area:</span> {selectedCountry.area} km²
+                  </li>
+               </ul>
+
+               <Link to="/" className="absolute top-2 right-3 mt-auto inline-block text-center hover:bg-indigo-700 text-indigo-500 hover:text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out dark:hover:bg-indigo-600">
+                  Back Home
+               </Link>
+            </div>
+         </div>
       </div>
    );
 }
